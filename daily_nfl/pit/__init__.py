6747b872203f2ac241b6e86df428bd3266117c37
Francisco Rodriguez <128325927+OneVillage83@users.noreply.github.com>
2026-08-20 22:52:30 -0700

    Export M5 PIT engine contracts

---
+++ daily_nfl/pit/__init__.py
@@ -0,0 +1,56 @@
+"""Historical point-in-time reconstruction engine for Daily NFL."""
+
+from daily_nfl.pit.availability import (
+    AvailabilityEvidence,
+    IndefensibleAvailabilityError,
+    derive_knowledge_timestamp,
+)
+from daily_nfl.pit.contracts import (
+    PITHorizon,
+    PITInputKind,
+    PITInputRef,
+    PITLeakageCode,
+    PITLeakageViolation,
+    PITPolicy,
+    PredictionCutoff,
+)
+from daily_nfl.pit.leakage import PITLeakageError, assert_no_leakage, find_leakage
+from daily_nfl.pit.repository import ScheduleStateAsOf, schedule_state_as_of
+from daily_nfl.pit.selector import (
+    PITObservation,
+    PITSelectionConflictError,
+    is_input_eligible,
+    select_latest_as_of,
+)
+from daily_nfl.pit.snapshot import (
+    PITSnapshotConflictError,
+    PITSnapshotManifest,
+    build_snapshot_manifest,
+    record_snapshot,
+)
+
+__all__ = [
+    "AvailabilityEvidence",
+    "IndefensibleAvailabilityError",
+    "PITHorizon",
+    "PITInputKind",
+    "PITInputRef",
+    "PITLeakageCode",
+    "PITLeakageError",
+    "PITLeakageViolation",
+    "PITObservation",
+    "PITPolicy",
+    "PITSelectionConflictError",
+    "PITSnapshotConflictError",
+    "PITSnapshotManifest",
+    "PredictionCutoff",
+    "ScheduleStateAsOf",
+    "assert_no_leakage",
+    "build_snapshot_manifest",
+    "derive_knowledge_timestamp",
+    "find_leakage",
+    "is_input_eligible",
+    "record_snapshot",
+    "schedule_state_as_of",
+    "select_latest_as_of",
+]
